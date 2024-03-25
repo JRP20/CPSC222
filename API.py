24cb5ff0e27a26aea91fa113abdd80e0f2816ea6
jxp414 <jxp414@CPCS222.myguest.virtualbox.org>
2024-03-24 21:09:19 -0400

    Pythod Code

--- API.py
+++ API.py
@@ -9,17 +9,17 @@ PASSWORD = 'abcABC123'
 
 def authenticate():
     auth = request.authorization
-    if not auth or auth.username != USERNAME or auth.password != PASSWORD:
+    if not auth or aut.username != USERNAME or auth.password != PASSWORD:
         return False
     return True
 
-@app.route('/api/users', methods=['POST'])
+@app.route('/api/users/', methods=['Post'])
 def users():
-    if request.method != 'POST':
+    if request.method != 'Post':
         return jsonify({'error': 'Method Not Allowed'}), 405
     if not authenticate():
-        return jsonify({'error': 'Unauthorized access1111'}),401
-    users_info={}
+        return jsonify({'error': 'Unauthorized access1111'}), 401
+    users_info = {}
     for user in pwd.getpwall():
         users_info[str(user.pw_uid)] = user.pw_name
         return jsonify(users_info)
@@ -27,14 +27,16 @@ def users():
 @app.route('/api/groups', methods=['POST'])
 def groups():
     if request.method != 'POST':
-        return jsonify ({'error': 'Method Not Allowed'}), 405
+        return jsonify({'error': 'Method Not Allowed'}), 405
     if not authenticate():
         return jsonify({'error': 'Unauthorized access2222'}), 401
-    groups_info={}
+    groups_info = {}
     for group in grp.getgrall():
         groups_info[str(group.gr_gid)] = group.gr_name
-    return jsonify(groups_info)
+        return jsonify(groups_info)
 
-if __name__=='__main__':
-    app.run(host='172.0.0.1',port=3000)
-
+@app.route('/')
+def index():
+    return send_file('HTTPS.html')
+if__name__==/__main__':
+    app.run(host='127.0.0.1,port=3000)
